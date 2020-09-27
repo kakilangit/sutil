530bfd1d9c33b860e13ab485edba86861c140b90
Firman Maulana <kakilangit@gmail.com>
2020-09-27 14:06:42 +0200

    Move examples to overview

--- error.go
+++ error.go
@@ -1,14 +1,16 @@
 package sutil
 
-// Error type will conform errorer interface and use it as error constant
+// Error type will conform errorer interface and use it as error constant.
 type Error string
 
-// Error method to conform errorer interface
+// Error method to conform errorer interface.
 func (e Error) Error() string {
 	return string(e)
 }
 
 const (
-	ErrInvalidStringSlice = Error("invalid string slice") // Invalid string slice
-	ErrInvalidLimit       = Error("invalid limit value")  // Invalid limit
+	// ErrInvalidStringSlice is invalid string slice
+	ErrInvalidStringSlice = Error("invalid string slice")
+	// ErrInvalidLimit is invalid limit
+	ErrInvalidLimit = Error("invalid limit value")
 )
