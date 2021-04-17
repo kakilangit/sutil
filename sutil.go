ae1fc16042449022aa5580ad927674fb2913c008
kakilangit <kakilangit@gmail.com>
2021-04-17 09:52:44 +0200

    Adds StructSlice features

--- sutil.go
+++ sutil.go
@@ -1,7 +1,10 @@
 package sutil
 
 import (
+	"errors"
+	"fmt"
 	"math"
+	"reflect"
 )
 
 // SplitStrings splits slice of string into slice of string slice with maximum member of string slice is limit.
@@ -73,4 +76,129 @@ func TotalPage(limit, length int) int {
 	}
 
 	return page
+}
+
+type StructSlice []interface{}
+
+// NoAllocStringMap will generate map[string]struct{} of string based on struct field name
+// This kind of hashmap is useful for uniqueness
+func (s StructSlice) NoAllocStringMap(name string) (map[string]struct{}, error) {
+	if s.isEmpty() {
+		return nil, nil
+	}
+
+	if !s.isValid() {
+		return nil, errors.New("invalid type of struct")
+	}
+
+	var res = make(map[string]struct{})
+
+	for i := range s {
+		val, err := s.getFieldStringValue(i, name)
+		if err != nil {
+			return nil, err
+		}
+		res[val] = struct{}{}
+	}
+
+	return res, nil
+}
+
+// StringSlice will generate slice of string based on struct field name
+func (s StructSlice) StringSlice(name string) ([]string, error) {
+	if s.isEmpty() {
+		return nil, nil
+	}
+
+	if !s.isValid() {
+		return nil, errors.New("invalid type of struct")
+	}
+
+	var list []string
+
+	for i := range s {
+		val, err := s.getFieldStringValue(i, name)
+		if err != nil {
+			return nil, err
+		}
+		list = append(list, val)
+	}
+
+	return list, nil
+}
+
+// StringSliceUnique will generate unique slice of string based on struct field name
+func (s StructSlice) StringSliceUnique(name string) ([]string, error) {
+	if s.isEmpty() {
+		return nil, nil
+	}
+
+	if !s.isValid() {
+		return nil, errors.New("invalid type of struct")
+	}
+
+	unique, err := s.NoAllocStringMap(name)
+	if err != nil {
+		return nil, err
+	}
+
+	var (
+		list = make([]string, len(unique))
+		i    = 0
+	)
+
+	for k := range unique {
+		list[0] = k
+		i++
+	}
+
+	return list, nil
+}
+
+func (s StructSlice) isValid() bool {
+	if s.isEmpty() {
+		return true
+	}
+
+	obj := s[0]
+	types := []reflect.Kind{reflect.Struct, reflect.Ptr}
+	for _, t := range types {
+		if reflect.TypeOf(obj).Kind() == t {
+			return true
+		}
+	}
+
+	return false
+}
+
+func (s StructSlice) isEmpty() bool {
+	return len(s) == 0
+}
+
+func (s StructSlice) getValue(i int) reflect.Value {
+	var (
+		val reflect.Value
+		obj = s[i]
+	)
+
+	switch reflect.TypeOf(obj).Kind() {
+	case reflect.Ptr:
+		val = reflect.ValueOf(obj).Elem()
+	default:
+		val = reflect.ValueOf(obj)
+	}
+
+	return val
+}
+
+func (s StructSlice) getFieldStringValue(i int, name string) (string, error) {
+	field := s.getValue(i).FieldByName(name)
+	if !field.IsValid() {
+		return "", fmt.Errorf("field %s not exists", name)
+	}
+	val, ok := field.Interface().(string)
+	if !ok {
+		return "", fmt.Errorf("field %s is not string", name)
+	}
+	return val, nil
 }
